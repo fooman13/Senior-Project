efb32c9451d8c77589c4bdd864fcaa799f7c2aba
Tanner Calliou <Tcalliou@live.com>
2022-09-12 16:40:48 -0700

    need to look at production UI to see how to make my stuff work

--- Assets/Scripts/Player/ChangeGravityDirection.cs
+++ Assets/Scripts/Player/ChangeGravityDirection.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ChangeGravityDirection : MonoBehaviour
 {
+    public GameObject Player;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,5 @@ public class ChangeGravityDirection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
